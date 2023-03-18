1356f12663465b5734d30bdbe53e26f2b6853146
MatheusStyle <matheusvazdev@gmail.com>
2023-03-18 01:39:03 -0300

    Finalizando css e agregando javascript na aplicação

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+    function trocaCor(cor){
+        let circulo = document.querySelector(".circulo")
+       
+
+        circulo.style.background = cor
+    }
+
+    function trocaImg(){
+        let imgIphone = document.querySelector("#Iphone")
+
+    }
